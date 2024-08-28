df4dc7505d568dfb1153497a552651a89e986d6c
Tina Wang <fabzidentiste@gmail.com>
2024-08-28 10:20:23 +0200

    🚀

---
+++ index.js
@@ -0,0 +1,29 @@
+/**
+ * @description Return error by value is the only way
+ * @example
+ * const { data: albums, error } = await $tryAsync(fetch("/albums"))
+ * @param {Promise} promise
+ * @returns {Promise<{ data: any | undefined, error: { name: string, message: string } | undefined }>}
+ */
+export async function $tryAsync(promise) {
+  try {
+    return { data: await promise, error: null }
+  } catch (error) {
+    return { data: null, error }
+  }
+}
+
+/**
+ * @description Return error by value is the only way
+ * @example
+ * const { data: album, error } = $try(myFunction, "albums", "DaftPunk", 2001)
+ * @param {Function} fct
+ * @returns {{ data: any, error: Error }}
+ */
+export function $try(fct, ...args) {
+  try {
+    return { data: fct(...args), error: null }
+  } catch (error) {
+    return { data: null, error }
+  }
+}
